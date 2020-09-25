6e4f5da817ed431c1e118df3149952e4a8b6ab8a
Nikhilchandra Rathod <nikhil.rathod@outlook.com>
2020-09-25 13:19:10 +0530

    Setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
